bfca1ad62052b38738baeefa38482430ab252039
Kwong-Hung YIP <kwonghung.yip@gmail.com>
2018-12-12 01:47:37 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-mkdir gradle_home
+mkdir gradle_cache
 
 docker run --rm -it \
   -v `pwd`:/usr/local/project \
-  -v `pwd`/gradle_home:/opt/gradle \
+  -v `pwd`/gradle_cache:/home/gradle/.gradle \
   -w /usr/local/project \
   gradle:4.10-jdk8 \
   gradle build -x test
